d0dfb2e8de6573718df424df718d7feee35bcd34
Nadya Sytchova <nadya_slcode@outlook.com>
2019-05-02 19:58:22 +0300

    Populate sample books

--- src/services/dummy-book-service.js
+++ src/services/dummy-book-service.js
@@ -1,254 +1,174 @@
 export default class DummyBookService {
     _books = [
         {
-            "id": "PZE9RP_rbekC",
-            "authors": [
-                "Alicia Ponte-Sucre"
-            ],
-            "title": "ABC Transporters in Microorganisms",
-            "pageCount": 260,
-            "publishedDate": "2009",
-            "img": "http://books.google.com/books/content?id=PZE9RP_rbekC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+          "id": "sTA2DwAAQBAJ",
+          "authors": "George R.R. Martin",
+          "title": "A Game of Thrones (A Song of Ice and Fire)",
+          "pageCount": 896,
+          "img": "http://books.google.com/books/content?id=sTA2DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
         },
         {
-            authors: ["Андрей Жвалевский", "Евгения Пастернак"],
-            id: "RblQDwAAQBAJ",
-            img: "http://books.google.com/books/content?id=RblQDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
-            pageCount: 181,
-            publishedDate: "2018-02-28",
-            title: "Минус один"
+          "id": "OYtkbGl2j0sC",
+          "authors": "Ray Bradbury",
+          "title": "Fahrenheit 451",
+          "pageCount": 208,
+          "img": "http://books.google.com/books/content?id=OYtkbGl2j0sC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
         },
         {
-            authors: ["Вадим Пугач"],
-            id: "7phGDwAAQBAJ",
-            img: "http://books.google.com/books/content?id=7phGDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
-            pageCount: undefined,
-            publishedDate: "2018-01-12",
-            title: "Заговор букв"
+          "id": "nkalO3OsoeMC",
+          "authors": "George Orwell",
+          "title": "Animal Farm",
+          "pageCount": 144,
+          "img": "http://books.google.com/books/content?id=nkalO3OsoeMC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
         },
         {
-            authors: ["Accomazzo Anthony", "Murray Nathaniel", "Lerner Ari"],
-            id: "ppjUtAEACAAJ",
-            img: "http://books.google.com/books/content?id=ppjUtAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
-            pageCount: 836,
-            publishedDate: "2017-03",
-            title: "Fullstack React"
+          "id": "Bd_UZAdvUDIC",
+          "authors": "William Gibson",
+          "title": "Neuromancer",
+          "pageCount": 288,
+          "img": "http://books.google.com/books/content?id=Bd_UZAdvUDIC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
         },
         {
-            authors: ["Artemij Fedosejev"],
-            id: "Rhl1CgAAQBAJ",
-            img: "http://books.google.com/books/content?id=Rhl1CgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
-            pageCount: 208,
-            publishedDate: "2015-08-27",
-            title: "React.js Essentials"
+          "id": "gnwETwF8Zb4C",
+          "authors": "Robert A. Heinlein",
+          "title": "Stranger in a Strange Land",
+          "pageCount": 300,
+          "img": "http://books.google.com/books/content?id=gnwETwF8Zb4C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
         },
         {
-            authors: ["Sven A. Robbestad"],
-            id: "SM-qDQAAQBAJ",
-            img: "http://books.google.com/books/content?id=SM-qDQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
-            pageCount: 422,
-            publishedDate: "2016-07-29",
-            title: "ReactJS Blueprints"
+          "id": "MYoDAQAAQBAJ",
+          "authors": "Octavia Butler",
+          "title": "Kindred",
+          "pageCount": 264,
+          "img": "http://books.google.com/books/content?id=MYoDAQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
         },
         {
-            authors: ["Brad Green", "Shyam Seshadri"],
-            id: "EjAhSZpynsYC",
-            img: "http://books.google.com/books/content?id=EjAhSZpynsYC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
-            pageCount: 196,
-            publishedDate: "2013-04-08",
-            title: "AngularJS"
+          "id": "HtDpW3qwc88C",
+          "authors": "Arthur C. Clarke",
+          "title": "2001: A Space Odyssey",
+          "pageCount": 288,
+          "img": "http://books.google.com/books/content?id=HtDpW3qwc88C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
         },
         {
-            authors: ["Ken Williamson"],
-            id: "8bd1BwAAQBAJ",
-            img: "http://books.google.com/books/content?id=8bd1BwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
-            pageCount: 212,
-            publishedDate: "2015-03-18",
-            title: "Learning AngularJS"
+          "id": "_zSzAwAAQBAJ",
+          "authors": "Suzanne Collins",
+          "title": "The Hunger Games",
+          "pageCount": 464,
+          "img": "http://books.google.com/books/content?id=_zSzAwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
         },
         {
-            "id": "ausMtbJtWVEC",
-            "authors": [
-                "Savannah Sunderman"
-            ],
-            "title": "A-b-c",
-            "pageCount": 26,
-            "publishedDate": "2009-08-01",
-            "img": "http://books.google.com/books/content?id=ausMtbJtWVEC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+          "id": "8qs1-ypf7e0C",
+          "authors": "Ernest Cline",
+          "title": "Ready Player One",
+          "pageCount": 384,
+          "img": "http://books.google.com/books/content?id=8qs1-ypf7e0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
         },
         {
-            "id": "M5HD9sv4xnoC",
-            "title": "Neues A B C Buch für Kinder. Mit Kupfern",
-            "publishedDate": "1780",
-            "img": "http://books.google.com/books/content?id=M5HD9sv4xnoC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+          "id": "k53eZ2ARZPwC",
+          "authors": "Walter M. Miller",
+          "title": "A Canticle for Leibowitz",
+          "pageCount": 352,
+          "img": "http://books.google.com/books/content?id=k53eZ2ARZPwC&printsec=frontcover&img=1&zoom=1&source=gbs_api"
         },
         {
-            "id": "mFezCQAAQBAJ",
-            "authors": [
-                "Николай Комлев"
-            ],
-            "title": "Самоучитель игры на Паскале. ABC и немного Турбо",
-            "publishedDate": "2017-09-05",
-            "img": "http://books.google.com/books/content?id=mFezCQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+          "id": "A4LZwFCK05UC",
+          "authors": "Jules Verne",
+          "title": "20,000 Leagues under the Sea",
+          "pageCount": 568,
+          "img": "http://books.google.com/books/content?id=A4LZwFCK05UC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
         },
         {
-            "id": "BMRUAAAAcAAJ",
-            "authors": [
-                "[Anonymus AC09747798]"
-            ],
-            "title": "Bukvar narodnogo ruskogo jazyka. (Abc-Buch der russischen Volkssprache in Galizien und Lodomerien.)",
-            "publishedDate": "1845",
-            "img": "http://books.google.com/books/content?id=BMRUAAAAcAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+          "id": "bQlHAAAAYAAJ",
+          "authors": "Herbert George Wells",
+          "title": "The War of the Worlds",
+          "pageCount": 303,
+          "img": "http://books.google.com/books/content?id=bQlHAAAAYAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
         },
         {
-            "id": "a4dux2ITWjcC",
-            "authors": [
-                "Kenneth Stanley Inglis"
-            ],
-            "title": "This is the ABC",
-            "pageCount": 525,
-            "publishedDate": "2006",
-            "img": "http://books.google.com/books/content?id=a4dux2ITWjcC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+          "id": "u4R_FstZDEgC",
+          "authors": "Dan Simmons",
+          "title": "Hyperion",
+          "pageCount": 496,
+          "img": "http://books.google.com/books/content?id=u4R_FstZDEgC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
         },
         {
-            "id": "gEz1bLeWb24C",
-            "authors": [
-                "Lyn Wendon",
-                "Domenica De Rosa"
-            ],
-            "title": "ABC",
-            "pageCount": 64,
-            "publishedDate": "2003-04-01",
-            "img": "http://books.google.com/books/content?id=gEz1bLeWb24C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+          "id": "2vnbMzYXBQsC",
+          "authors": "Isaac Asimov",
+          "title": "I, Robot",
+          "pageCount": 304,
+          "img": "http://books.google.com/books/content?id=2vnbMzYXBQsC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
         },
         {
-            "id": "IqFKCAAAQBAJ",
-            "authors": [
-                "Шапошникова Тамара Евгеньевна"
-            ],
-            "title": "ABC. Английский алфавит. 26 карточек 3+",
-            "pageCount": 32,
-            "publishedDate": "2014-02-05",
-            "img": "http://books.google.com/books/content?id=IqFKCAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+          "id": "KHsHltMFsRAC",
+          "authors": "Joe Haldeman",
+          "title": "The Forever War",
+          "pageCount": 236,
+          "img": "http://books.google.com/books/content?id=KHsHltMFsRAC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
         },
         {
-            "id": "eEoYQA4QD_wC",
-            "authors": [
-                "Graeme P. Currie"
-            ],
-            "title": "ABC of COPD",
-            "pageCount": 88,
-            "publishedDate": "2010-11-04",
-            "img": "http://books.google.com/books/content?id=eEoYQA4QD_wC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+          "id": "_oG_iTxP1pIC",
+          "authors": "Daniel Keyes",
+          "title": "Flowers for Algernon",
+          "pageCount": 304,
+          "img": "http://books.google.com/books/content?id=_oG_iTxP1pIC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
         },
         {
-            "id": "ckmi63u4howC",
-            "authors": [
-                "Bernard Valman",
-                "Roslyn Thomas"
-            ],
-            "title": "ABC of the First Year",
-            "pageCount": 136,
-            "publishedDate": "2009-04-15",
-            "img": "http://books.google.com/books/content?id=ckmi63u4howC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+          "id": "o79lk6nTsRgC",
+          "authors": "Margaret Atwood",
+          "title": "The Handmaid's Tale",
+          "pageCount": 336,
+          "img": "http://books.google.com/books/content?id=o79lk6nTsRgC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
         },
         {
-            "id": "rfkix2OQLAcC",
-            "authors": [
-                "Самков Геннадий Алексеевич"
-            ],
-            "title": "jQuery. Сборник рецептов",
-            "pageCount": 416,
-            "publishedDate": "2010",
-            "img": "http://books.google.com/books/content?id=rfkix2OQLAcC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+          "id": "v-IoDwAAQBAJ",
+          "authors": "Stephen King",
+          "title": "The Green Mile",
+          "pageCount": 512,
+          "img": "http://books.google.com/books/content?id=v-IoDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
         },
         {
-            "id": "9rCADQAAQBAJ",
-            "authors": [
-                "Антон Шевчук"
-            ],
-            "title": "jQuery для начинающих",
-            "pageCount": 156,
-            "img": "http://books.google.com/books/content?id=9rCADQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+          "id": "8JyRntokmqYC",
+          "authors": "Raychel Haugrud Reiff",
+          "title": "Aldous Huxley",
+          "pageCount": 144,
+          "img": "http://books.google.com/books/content?id=8JyRntokmqYC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
         },
         {
-            "id": "KbRX97KyvQgC",
-            "authors": [
-                "Бенедетти Р"
-            ],
-            "title": "Изучаем работу с jQuery",
-            "pageCount": 512,
-            "publishedDate": "2012-06-07",
-            "img": "http://books.google.com/books/content?id=KbRX97KyvQgC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+          "id": "einYDQAAQBAJ",
+          "authors": "H. G. Wells",
+          "title": "The Time Machine",
+          "pageCount": 144,
+          "img": "http://books.google.com/books/content?id=einYDQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
         },
         {
-            "id": "8VUHCgAAQBAJ",
-            "authors": [
-                "Кит Вуд"
-            ],
-            "title": "Расширение библиотеки jQuery",
-            "publishedDate": "2017-09-05",
-            "img": "http://books.google.com/books/content?id=8VUHCgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+          "id": "IwywDY4P6gsC",
+          "authors": "Isaac Asimov",
+          "title": "Foundation",
+          "pageCount": 320,
+          "img": "http://books.google.com/books/content?id=IwywDY4P6gsC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
         },
         {
-            "id": "ztcyDwAAQBAJ",
-            "authors": [
-                "Джон Дакетт"
-            ],
-            "title": "Javascript и jQuery. Интерактивная веб-разработка",
-            "publishedDate": "2017-09-05",
-            "img": "http://books.google.com/books/content?id=ztcyDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+          "id": "FTHnGZeroUoC",
+          "authors": "George Orwell",
+          "title": "Nineteen Eighty-Four",
+          "pageCount": 416,
+          "img": "http://books.google.com/books/content?id=FTHnGZeroUoC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
         },
         {
-            "id": "yCMCJmQi75UC",
-            "authors": [
-                "Самков Геннадий Алексеевич"
-            ],
-            "title": "jQuery. Сборник рецептов (+ CD) (2-е издание)",
-            "pageCount": 416,
-            "publishedDate": "2011",
-            "img": "http://books.google.com/books/content?id=yCMCJmQi75UC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+          "id": "p1MULH7JsTQC",
+          "authors": "Frank Herbert",
+          "title": "Dune",
+          "pageCount": 896,
+          "img": "http://books.google.com/books/content?id=p1MULH7JsTQC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
         },
         {
-            "id": "1t6eAwAAQBAJ",
-            "authors": [
-                "Крис Сейболд"
-            ],
-            "title": "jQuery Mobile: разработка приложений для смартфонов и планшетов",
-            "pageCount": 256,
-            "publishedDate": "2013",
-            "img": "http://books.google.com/books/content?id=1t6eAwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-        },
-        {
-            "id": "1Q2KkgAACAAJ",
-            "authors": [
-                "Стивен Хольцнер"
-            ],
-            "title": "JQuery",
-            "pageCount": 223,
-            "publishedDate": "2010"
-        },
-        {
-            "id": "U7bbaax2FDUC",
-            "authors": [
-                "Каслдайн Э"
-            ],
-            "title": "Изучаем jQuery. 2-е изд.",
-            "pageCount": 400,
-            "publishedDate": "2012-07-23",
-            "img": "http://books.google.com/books/content?id=U7bbaax2FDUC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-        },
-        {
-            "id": "7kfyzf2BnP0C",
-            "authors": [
-                "Cody Lindley"
-            ],
-            "title": "jQuery Cookbook",
-            "pageCount": 480,
-            "publishedDate": "2009-11-09",
-            "img": "http://books.google.com/books/content?id=7kfyzf2BnP0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+          "id": "j24GMN0OtS8C",
+          "authors": "Douglas Adams",
+          "title": "The Hitchhiker's Guide to the Galaxy",
+          "pageCount": 304,
+          "img": "http://books.google.com/books/content?id=j24GMN0OtS8C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
         }
-    ]
+      ];
 
     getBooks = async () => {
         return this._books;
